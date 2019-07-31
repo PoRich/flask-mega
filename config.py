7527aae54455264b4cbaf231f0a0a1b2ff480ea3
Poor Richard <41810749+PoRich@users.noreplay.github.com>
2019-07-31 17:37:57 -0400

    heroku deployment changes

--- config.py
+++ config.py
@@ -34,3 +34,17 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
 
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
+
+class ProductionConfig(Config):
+    # NOT IN USE - need to configure app factory to use APP_SETTINGS
+    FLASK_APP = wsgi.py
+
+    DEBUG = 0
+    DEVELOPMENT = 0
+    LOG_TO_STDOUT = 1
+
+    DATABASE_URL = postgres://ijukbfnwqsckxp:5f35e6f8e73079f1e7b6a9799b12cf2935183f8f90ac4b9b286bc5b5935d3b61@ec2-50-16-225-96.compute-1.amazonaws.com:5432/d5vvard2u9haa3
+    ELASTICSEARCH_URL = https://paas:9b63e550a6ea0bf4ada40ffb1a5cb59a@thorin-us-east-1.searchly.com
