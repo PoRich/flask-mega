fc306653b89c5be8d484f5f4d83d6b0503b8d0d4
Poor Richard <41810749+PoRich@users.noreplay.github.com>
2019-08-05 11:53:51 -0400

    ADMINS

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = os.environ.get('ADMINS')
+    ADMINS = ['info@bedfordap.com']
     # list of emails to receive error reports
 
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
