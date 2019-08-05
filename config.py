44972668ba0f923340befac59fd544a55d58ce65
Poor Richard <41810749+PoRich@users.noreplay.github.com>
2019-08-05 11:20:14 -0400

    cleanse config vars

--- config.py
+++ config.py
@@ -24,10 +24,9 @@ class Config(object):
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['info@bedfordap.com']
+    ADMINS = os.environ.get('ADMINS')
     # list of emails to receive error reports
 
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
@@ -45,3 +44,5 @@ class ProductionConfig(Config):
     DEBUG = 0
     LOG_TO_STDOUT = 1
     WEB_CONCURRENCY = 4
+
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
